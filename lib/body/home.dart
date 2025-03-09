a07f93d29d3515d4c1c3cb483899891e876f9abb
João Pedro Souza Arruda <joao.arruda.jope16@gmail.com>
2025-03-09 17:13:20 -0300

    home arco update

--- lib/body/home.dart
+++ lib/body/home.dart
@@ -21,28 +21,18 @@ class HomePage extends StatelessWidget {
           ),
           // Novo Container sobreposto
           Positioned(
-            top: 50, // Ajuste a posição conforme necessário
-            left: MediaQuery.of(context).size.width * 0.5, // Ajuste a posição conforme necessário
+            top: 70, // Ajuste a posição conforme necessário
+            left: MediaQuery.of(context).size.width * 0.35, // Ajuste a posição conforme necessário
             child: Container(
-              width: 150,
-              height: 100,
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 1.2,
               decoration: BoxDecoration(
-                color: const Color.fromARGB(0, 255, 255, 255),
+                color: Colors.transparent,
                 borderRadius: BorderRadius.circular(100),
-                border: const Border(
-                  left: BorderSide(
-                    color: Color(0xffD0DEE7),
-                    width: 10
-                  )
+                border: Border.all(
+                  color: Color(0xFFCCCCCC),
+                  width: 10,
                 ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.3),
-                    spreadRadius: 2,
-                    blurRadius: 5,
-                    offset: Offset(0, 3),
-                  ),
-                ],
               ),
             ),
           ),
