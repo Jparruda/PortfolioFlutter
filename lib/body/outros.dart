bee4d948376f203894b6d05eda240a44842f7c46
João Pedro Souza Arruda <joao.arruda.jope16@gmail.com>
2025-03-09 16:56:17 -0300

    outros (update)

--- lib/body/outros.dart
+++ lib/body/outros.dart
@@ -7,9 +7,21 @@ class Outros extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      alignment: Alignment.bottomCenter,
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height,
       color: Color(0xFF000000),
+      child: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.6,
+            decoration: const BoxDecoration(
+              color: Color(0xff0E1011),
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(70), topRight: Radius.circular(70)),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
